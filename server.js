4d7abee35d674be78edbe34dc07a431641929711
Glitch (brawny-mesquite-conga) <none>
2024-08-04 11:13:51 +0000

    🐃👆 Updated with Glitch

--- server.js
+++ server.js
@@ -1,128 +1,129 @@
-var google = require('googleapis').google;
-var sheets = google.sheets('v4');
+var google = require("googleapis").google;
+var sheets = google.sheets("v4");
 var userName;
 var dataDeets;
 
 // the process.env values are set in .env
 var clientID = process.env.CLIENT_ID;
 var clientSecret = process.env.CLIENT_SECRET;
-var callbackURL = 'https://'+process.env.PROJECT_DOMAIN+'.glitch.me/login/google/return';
-var scopes = ['https://www.googleapis.com/auth/spreadsheets'];
+var callbackURL =
+  "https://" + process.env.PROJECT_DOMAIN + ".glitch.me/login/google/return";
+var scopes = ["https://www.googleapis.com/auth/spreadsheets"];
 var oauth2Client = new google.auth.OAuth2(clientID, clientSecret, callbackURL);
 
 var url = oauth2Client.generateAuthUrl({
   // 'online' (default) or 'offline' (gets refresh_token)
-  access_type: 'offline',
+  access_type: "offline",
   // If you only need one scope you can pass it as a string
   scope: scopes,
   // Force getting a refresh token
-  prompt: 'consent'
+  prompt: "consent",
 });
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
-var expressSession = require('express-session');
+var expressSession = require("express-session");
 
 // cookies are used to save authentication
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-app.use(express.static('views'))
-app.use(bodyParser.urlencoded({extended: true}));
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+app.use(express.static("views"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(expressSession({ secret:'watchingmonkeys', resave: true, saveUninitialized: true }));
+app.use(
+  expressSession({
+    secret: "watchingmonkeys",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-if(process.env.REFRESH_TOKEN) {
+if (process.env.REFRESH_TOKEN) {
   oauth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN
+    refresh_token: process.env.REFRESH_TOKEN,
   });
 }
 
 // index route
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // on clicking "logoff" the cookie is cleared
-app.get('/logoff',
-  function(req, res) {
-    res.clearCookie('google-auth');
-    res.redirect('/');
-  }
-);
+app.get("/logoff", function (req, res) {
+  res.clearCookie("google-auth");
+  res.redirect("/");
+});
 
-app.get('/auth/google', function(req, res) {
+app.get("/auth/google", function (req, res) {
   res.redirect(url);
 });
 
-app.get('/login/google/return', function(req, res) {
-    oauth2Client.getToken(req.query.code, function (err, tokens) {
-      console.log(tokens)
-      // Tokens contains an access_token and a refresh_token if you set access type to offline. Save them.
-      if (!err) {
-        oauth2Client.setCredentials({
-          access_token: tokens.access_token
-        });
-        res.redirect('/setcookie');
-      } else {
-        console.log("Aww, man: " + err);
-      }
-    });
-  }
-);
+app.get("/login/google/return", function (req, res) {
+  oauth2Client.getToken(req.query.code, function (err, tokens) {
+    console.log(tokens);
+    // Tokens contains an access_token and a refresh_token if you set access type to offline. Save them.
+    if (!err) {
+      oauth2Client.setCredentials({
+        access_token: tokens.access_token,
+      });
+      res.redirect("/setcookie");
+    } else {
+      console.log("Aww, man: " + err);
+    }
+  });
+});
 
 // on successful auth, a cookie is set before redirecting
 // to the success view
-app.get('/setcookie',
-  function(req, res) {
-    res.cookie('google-auth', new Date());
-    res.redirect('/success');
-  }
-);
+app.get("/setcookie", function (req, res) {
+  res.cookie("google-auth", new Date());
+  res.redirect("/success");
+});
 
 // if cookie exists, success. otherwise, user is redirected to index
-app.get('/success',
-  function(req, res) {
-    if(req.cookies['google-auth']) {
-      res.sendFile(__dirname + '/views/success.html');
-    } else {
-      res.redirect('/');
-    }
+app.get("/success", function (req, res) {
+  if (req.cookies["google-auth"]) {
+    res.sendFile(__dirname + "/views/success.html");
+  } else {
+    res.redirect("/");
   }
-);
+});
 
-app.post('/spreadsheet', function(req, res) {
+app.post("/spreadsheet", function (req, res) {
   var body = req.body;
-  if(body.secret_key !== process.env.SECRET_KEY) {
-    return res.status(403).send("Incorrect secret key"); 
+  if (body.secret_key !== process.env.SECRET_KEY) {
+    return res.status(403).send("Incorrect secret key");
   }
-  if(!body.data) {
-    return res.status(400).send("No data included"); 
+  if (!body.data) {
+    return res.status(400).send("No data included");
   }
-  if(!body.spreadsheet_key) {
+  if (!body.spreadsheet_key) {
     return res.status(400).send("No spreadsheet key included!");
   }
-  sheets.spreadsheets.values.append({
+  sheets.spreadsheets.values.append(
+    {
       spreadsheetId: body.spreadsheet_key,
-      range: body.spreadsheet_range || 'Sheet1',
-      valueInputOption: 'RAW',
-      insertDataOption: 'INSERT_ROWS',
+      range: body.spreadsheet_range || "Sheet1",
+      valueInputOption: "RAW",
+      insertDataOption: "INSERT_ROWS",
       resource: {
-        values: [
-          body.data
-        ],
+        values: [body.data],
       },
-      auth: oauth2Client
-    }, (err, response) => {
+      auth: oauth2Client,
+    },
+    (err, response) => {
       if (err) {
         res.status(500).send(err);
         return console.error(err);
       }
       return res.status(200).send(response);
-    })
+    }
+  );
 });
 
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
